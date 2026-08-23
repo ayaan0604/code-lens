60ace985a1035db8f510ffca7ece358f7e2799c6
ayaan0604 <ansariayaan0604@gmail.com>
2026-08-23 23:42:47 +0530

    implement imports

--- astAnalyzer.py
+++ astAnalyzer.py
@@ -1,13 +1,50 @@
 from models import *
 import ast
+from pprint import pp
 
 
-class PythonASTAnalyzer:
+class PythonASTAnalyzer(ast.NodeVisitor):
     def __init__(self, source: str, file_path: str):
+        super().__init__()
         self.source = source
         self.file_path = file_path
         self.tree = ast.parse(source)
+        self.analyzedFile = AnalyzedFile(None)
+        
+    #visitor functions
+    def visit_Import(self, node):
+        for alias in node.names:
+            moduleName = alias.name
+            importedName = alias.name
+            asName = alias.asname
+            lineNo = node.lineno
 
+            self.analyzedFile.imports.append(
+                ImportInfo(
+                    module=moduleName,
+                    imported_name = importedName,
+                    alias= asName,
+                    line_number= lineNo
+                )
+            )
+
+    def visit_ImportFrom(self, node):
+        module = node.module
+        line = node.lineno
+        for alias in node.names:
+            importedName = module + "." + alias.name
+            asName = alias.asname  
+            self.analyzedFile.imports.append(
+                ImportInfo(
+                    module= module,
+                    imported_name= importedName,
+                    alias = asName,
+                    line_number=line
+                )
+            )
+                
+
+    #analyzer functions
     def get_metadata(self):
         path = self.file_path
         language = "python" #for now
@@ -31,25 +68,28 @@ class PythonASTAnalyzer:
             code_lines=code_lines
         )
             
+    def getImports(self) -> List[ImportInfo]:
+        pass
 
     def analyze(self) -> AnalyzedFile:
 
-        metadata = self.get_metadata()
+        self.analyzedFile.metadata = self.get_metadata()
+
+        self.visit(self.tree)
 
 
-        return AnalyzedFile(
-            metadata
-        )
+        return self.analyzedFile
 
 source = """
 import os
+from x import y as z
 
     #this is a comment
  
 def hello():
     print("hello")
 """
-analyzer = ASTAnalyzer(source, "temp.py")
+analyzer = PythonASTAnalyzer(source, "temp.py")
 result = analyzer.analyze()
 
-print(result)
+pp(result)
