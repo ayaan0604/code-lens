e825c38ade9d0117c2531521283c1ed2e0138ebd
ayaan0604 <ansariayaan0604@gmail.com>
2026-08-24 01:16:43 +0530

    implement class

--- astAnalyzer.py
+++ astAnalyzer.py
@@ -11,8 +11,72 @@ class PythonASTAnalyzer(ast.NodeVisitor):
         self.tree = ast.parse(source)
         self.analyzedFile = AnalyzedFile(None)
         self.scope = []
+
+    #helper functions
+    def getParameters(self, args: ast.arguments)->List[ParameterInfo]:
+            parameters :List[ParameterInfo] = []
+            for arg in args.args:
+                pname = arg.arg
+                pann = None if arg.annotation is None else ast.unparse(arg.annotation) 
+                default = None
+                parameters.append(ParameterInfo(pname, default, pann))
+    
+            idx = 1
+            for default in args.defaults:
+                parameters[-idx].default = ast.unparse(default) if default is not None else default
+                idx+=1
+    
+            return parameters
+
+    def getFunctionInfo(self, node):
+        name = node.name
+        lineStart = node.lineno
+        lineEnd = node.end_lineno
+        sourceCode = ast.get_source_segment(self.source, node)
         
+        parameters = self.getParameters(node.args)
+
+        qualified_name = ".".join(self.scope + [name])
+
+        parent = self.scope[-1] if self.scope else None
+
+        returnaAnnotation = ast.unparse(node.returns) if node.returns else None
+
+        return FunctionInfo(
+            name,
+            qualified_name,
+            lineStart,
+            lineEnd,
+            sourceCode,
+            parent,
+            returnaAnnotation,
+            parameters
+        )
+
+
+    def getClassInfo(self, node:ast.ClassDef):
+        name = node.name
+
+        qualified_name = ".".join(self.scope + [name])
+
+        lineNumber = node.lineno
+
+        bases = [ast.unparse(b) for b in node.bases]
+
+        methods = []
+
+        return ClassInfo(
+            name = name,
+            qualified_name= qualified_name,
+            line_number=lineNumber,
+            bases= bases,
+            methods= methods
+        )
+
+
+    
     #visitor functions
+
     def visit_Import(self, node):
         for alias in node.names:
             moduleName = alias.name
@@ -44,50 +108,33 @@ class PythonASTAnalyzer(ast.NodeVisitor):
                 )
             )
 
-    def getParameters(self, args: ast.arguments)->List[ParameterInfo]:
-        parameters :List[ParameterInfo] = []
-        for arg in args.args:
-            pname = arg.arg
-            pann = None if arg.annotation is None else ast.unparse(arg.annotation) 
-            default = None
-            parameters.append(ParameterInfo(pname, default, pann))
-
-        idx = 1
-        for default in args.defaults:
-            parameters[-idx].default = ast.unparse(default) if default is not None else default
-            idx+=1
-
-        return parameters
+    
 
     def visit_FunctionDef(self, node):
-        name = node.name
-        lineStart = node.lineno
-        lineEnd = node.end_lineno
-        sourceCode = ast.get_source_segment(self.source, node)
-        
-        parameters = self.getParameters(node.args)
+        info = self.getFunctionInfo(node)
 
-        qualified_name = ".".join(self.scope + [name])
+        self.analyzedFile.functions.append(info)
 
-        parent = self.scope[-1] if self.scope else None
+        if self.scope:
+            parent = self.scope[-1]
+            for c in self.analyzedFile.classes:
+                if c.name == parent:
+                    c.methods.append(info)
 
-
-        returnaAnnotation = node.returns.id if node.returns else None
-
-        self.analyzedFile.functions.append(
-            FunctionInfo(
-                name,
-                qualified_name,
-                lineStart,
-                lineEnd,
-                sourceCode,
-                parent,
-                returnaAnnotation,
-                parameters
-            )
-        )
 
         self.generic_visit(node)
+
+    def visit_ClassDef(self, node):
+
+        info = self.getClassInfo(node)
+
+        self.analyzedFile.classes.append(info)
+
+        self.scope.append(node.name)
+        self.generic_visit(node)
+        self.scope.pop()
+
+    
         
     #analyzer functions
     def get_metadata(self):
@@ -128,7 +175,10 @@ import os
 from x import y as z
 
     #this is a comment
- 
+class A(b,C.d):
+    def login():
+        return true
+
 def hello(name:Optional[str], x=10, y=20)->str:
     print("hello")
     return name
